4584f29b527ead4998df5c0f5b4144d146f873c6
joshua hahn <tailrin.jh@gmail.com>
2019-12-19 19:42:41 -0700

    initial commit

--- moviedex.js
+++ moviedex.js
@@ -10,46 +10,22 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
-app.use(function validateBearerToken(req, res, next){
-    const authToken = req.get('Authorization')
-    const apiToken = process.env.API_TOKEN
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({ error: 'Unauthorized request' })
-    }
-    next()
-});
+
 
 const handleCountrySearch = (country, movies) => {
-    return movies.filter(movie => movie.country.toLowerCase() === country.toLowerCase())
+
 }
 
 const handleGenreSearch = (genre, movies) => {
-    return movies.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase())
+    
 }
 
 const handleAvgVoteSearch = (avg_vote, movies) => {
-    return movies.filter(movie => Number(movie.avg_vote)>= Number(avg_vote))
+    
 }
 
 const handleGet = (req, res) => {
-    const genre = req.query.genre;
-    const avg_vote = req.query.avg_vote;
-    const country = req.query.country;
-    let movies = moviedex.slice(0);
-
-    if(!!genre){
-        movies = handleGenreSearch(genre, movies);
-    }
-
-    if(!!avg_vote){
-        movies = handleAvgVoteSearch(avg_vote, movies);
-    }
-
-    if(!!country){
-        movies = handleCountrySearch(country, movies)
-    }
-
-    res.send(movies)
+    res.send('this is a test')
 }
 
 
