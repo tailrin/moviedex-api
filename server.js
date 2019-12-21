b9af8f15762f5cd8df1f5ba593b502a375cb30b8
joshua hahn <tailrin.jh@gmail.com>
2019-12-21 11:19:25 -0700

    prepped for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./moviedex.js');
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:5000`)
 });
